764a7dad63be0259199d6dfb4fa0c5672a51c585
David <arturo_preciado@live.com.mx>
2021-09-21 17:13:13 -0700

    test jest

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import PKG from './package.json';
 import Block from './src/blockchain/block';
 
-const { name, version, description, author } = PKG;
+const { name, version } = PKG;
+const {genesis} = Block;
 
-console.log(`${name} v${version} ${description} ${author}`);
+const block1 = Block.mine(genesis,'trasact1');
+console.log(block1.toString());
 
-const block = new Block(Date.now(), 'pr3v10-h4sh', 'h4sh', 'd4t4');
-console.log(block.toString());
+const block2 = Block.mine(block1,'trasact2');
+console.log(block2.toString());
--- src/blockchain/block.js
+++ src/blockchain/block.js
@@ -1,9 +1,28 @@
+
+import {SHA256} from 'crypto-js';
 class Block{
     constructor(timestamp, previousHash, hash, data){
         this.timestamp = timestamp;
         this.previousHash = previousHash;
         this.hash = hash;
         this.data = data;
+    }
+
+    static get genesis(){
+        const timestamp = (new Date(2000,0,1)).getTime();
+        return new this(timestamp, undefined,'g3n3s1s-h4sh', 'transaccion0');
+    }
+
+    static mine(previousBlock, data){
+        const timestamp = Date.now();
+        const {hash: previousHash} = previousBlock;
+        const hash = Block.hash(timestamp, previousHash, data);
+
+        return new this(timestamp, previousHash, hash, data);
+    }
+
+    static hash(timestamp, previousHash, data){
+        return SHA256(`${timestamp}${previousHash},${data}`).toString();
     }
 
     toString(){
@@ -13,7 +32,7 @@ class Block{
 
     return `Block -
     timestamp   : ${timestamp}
-    previoushash: ${previousHash}
+    previousHash: ${previousHash}
     hash        : ${hash}
     data        : ${data}
     `;
